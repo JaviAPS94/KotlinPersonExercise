5f2acd8e5d856d53c03b791960e726a0767ef5dd
Alex Pinaida <alex.pinaida@rappi.com>
2021-07-28 22:19:34 -0500

    feat: if expression and object equality

--- src/KotlinPerson.kt
+++ src/KotlinPerson.kt
@@ -8,20 +8,39 @@ data class KotlinPerson (
     val dateOfBirth: Calendar?
         )
 {
-    val age : Int
+    var favoriteColor : String? = null
+
+    fun getLastLetter (s: String) = s.takeLast(1)
+
+    fun getUpperCaseColor(): String {
+        return favoriteColor?.toUpperCase() ?: ""
+    }
+
+    fun getLastLetterOfColor(): String {
+        return favoriteColor?.let { getLastLetter(it) } ?: ""
+    }
+
+    fun getColorType(): String {
+        return when (getUpperCaseColor()) {
+            "" -> "empty"
+            "RED", "BLUE", "GREEN" -> "rgb"
+            else -> "other"
+        }
+    }
+
+    val safeAge : Int
+        get() = age ?: -1
+
+    val age : Int?
         get() = getAge(dateOfBirth)
 
     companion object {
-        fun getAge(dateOfBirth: Calendar?): Int {
-            if (dateOfBirth == null) return -1
+        fun getAge(dateOfBirth: Calendar?): Int? {
+            if (dateOfBirth == null) return null
 
             val today = GregorianCalendar()
             val years = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR)
-            return if (dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)) {
-                years - 1
-            } else {
-                years
-            }
+            return if (dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)) years - 1 else years
         }
     }
 
@@ -35,4 +54,6 @@ fun main(args: Array<String>) {
     println("$john's age is ${john.age}")
     println("$jane's age is ${jane.age}")
     println("The age of someone born on 3rd May 1988 is " + KotlinPerson.getAge(GregorianCalendar(1988, 5, 3)))
+    val olderPerson = if (john.safeAge > jane.safeAge) john else jane
+    println("The older person was $olderPerson")
 }
